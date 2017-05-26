00500e1d746be69d8d892ceb826a66ee36fb466e
Noah <xlash19o13@gmail.com>
2017-05-26 00:34:31 -0400

    Methods for admin and aliases

--- xlash.bot.khux/src/main/java/xlash/bot/khux/commands/CommandBase.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/commands/CommandBase.java
@@ -1,16 +1,20 @@
 package xlash.bot.khux.commands;
 
 import de.btobastian.javacord.entities.message.Message;
-import de.btobastian.sdcf4j.Command;
-import de.btobastian.sdcf4j.CommandExecutor;
 
-public abstract class CommandBase implements CommandExecutor{
+public abstract class CommandBase{
+	
+	public abstract String[] getAliases();
 	
 	public abstract void onCommand(String[] args, Message message);
 	
 	public abstract String getDescription();
 	
 	public abstract String getUsage();
+	
+	public boolean isAdmin(){
+		return false;
+	}
 	
 	public void printDescriptionUsage(Message message){
 		message.reply(this.getUsage() + "\n" + this.getDescription());
