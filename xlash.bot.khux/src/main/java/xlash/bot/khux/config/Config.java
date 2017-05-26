6a98277d9ab0832704e9fbfcbde35cc0946c2e0f
Noah <xlash19o13@gmail.com>
2017-05-26 00:34:45 -0400

    Import cleaning

--- xlash.bot.khux/src/main/java/xlash/bot/khux/config/Config.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/config/Config.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.Scanner;
 
 import xlash.bot.khux.GameEnum;
 
