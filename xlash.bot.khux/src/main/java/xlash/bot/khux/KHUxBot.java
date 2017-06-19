0d6fad0783561855253a878130f06d700e9227a8
Noah <xlash19o13@gmail.com>
2017-06-19 18:55:14 -0400

    Fixed NA lux times

--- xlash.bot.khux/src/main/java/xlash/bot/khux/KHUxBot.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/KHUxBot.java
@@ -32,7 +32,7 @@ import xlash.bot.khux.sheduler.TimedEvent;
 
 public class KHUxBot {
 
-	public static final String VERSION = "1.2.5";
+	public static final String VERSION = "1.2.6";
 
 	public static DiscordAPI api;
 
@@ -117,7 +117,7 @@ public class KHUxBot {
 
 			@Override
 			public String[] getTimes() {
-				return new String[]{"03:00:00", "09:00:00"};
+				return new String[]{"03:00:00", "09:00:00", "15:00:00", "21:00:00"};
 			}
 
 			@Override
@@ -135,7 +135,7 @@ public class KHUxBot {
 
 			@Override
 			public String[] getTimes() {
-				return new String[]{"04:00:00", "10:00:00"};
+				return new String[]{"04:00:00", "10:00:00", "16:00:00", "22:00:00"};
 			}
 
 			@Override
