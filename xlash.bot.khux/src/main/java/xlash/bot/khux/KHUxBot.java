4e5b81ce235164e244248eb869da44cf5aba7fd2
Noah <xlash19o13@gmail.com>
2017-06-25 22:49:21 -0400

    Version 1.2.7

--- xlash.bot.khux/src/main/java/xlash/bot/khux/KHUxBot.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/KHUxBot.java
@@ -32,7 +32,7 @@ import xlash.bot.khux.sheduler.TimedEvent;
 
 public class KHUxBot {
 
-	public static final String VERSION = "1.2.6";
+	public static final String VERSION = "1.2.7";
 
 	public static DiscordAPI api;
 
