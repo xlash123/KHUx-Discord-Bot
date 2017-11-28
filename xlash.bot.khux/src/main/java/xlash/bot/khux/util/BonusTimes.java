15362b8549a2459ee2dbf5994416bcfa8274bc41
Noah Gil <xlash19o13@gmail.com>
2017-11-27 22:44:34 -0500

    Does more time magic

--- xlash.bot.khux/src/main/java/xlash/bot/khux/util/BonusTimes.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/util/BonusTimes.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import xlash.bot.khux.GameEnum;
 import xlash.bot.khux.sheduler.Scheduler;
 
-public class LuxTimes {
+public class BonusTimes {
 	
 	public static String[] doubleLuxStartNA = new String[] {"02:00:00", "08:00:00", "14:00:00", "20:00:00"};
 	public static String[] doubleLuxStartJP = new String[] {"12:00:00", "22:00:00"};
@@ -20,25 +20,60 @@ public class LuxTimes {
 	public static String[] dailyRaidStartJP = new String[] {};
 	public static String[] dailyRaidStopJP = new String[] {};
 	
+	public static String[] uxBonusStartNA = new String[] {"04:00:00", "10:00:00", "14:00:00", "19:00:00", "23:00:00"};
+	public static String[] uxBonusEndNA = new String[] {"04:30:00", "10:30:00", "14:30:00", "19:30:00", "23:30:00"};
+	public static String[] uxBonusStartJP = new String[] {"19:00:00", "23:00:00"};
+	public static String[] uxBonusEndJP = new String[] {"19:30:00", "23:30:00"};
+	
 	/**
 	 * Gets the time difference in minutes for the next double lux time.
 	 * @param game
 	 * @return time difference in minutes
 	 */
-	public static int timeDifference(GameEnum game) {
+	public static int luxTimeDifference(GameEnum game) {
 		String[] start;
-		SimpleDateFormat sdf;
 		if(game.equals(GameEnum.NA)) {
 			start = doubleLuxStartNA;
-			sdf = Scheduler.SDF_NA;
 		}else {
 			start = doubleLuxStartJP;
+		}
+		
+		return timeDifference(game, start);
+	}
+	
+	public static int uxTimeDifference(GameEnum game) {
+		String[] start;
+		if(game.equals(GameEnum.NA)) {
+			start = uxBonusStartNA;
+		}else {
+			start = uxBonusStartJP;
+		}
+		
+		return timeDifference(game, start);
+	}
+	
+	public static int raidTimeDifference(GameEnum game) {
+		String[] start;
+		if(game.equals(GameEnum.NA)) {
+			start = dailyRaidStartNA;
+		}else {
+			start = dailyRaidStartJP;
+		}
+		
+		return timeDifference(game, start);
+	}
+	
+	private static int timeDifference(GameEnum game, String[] times) {
+		SimpleDateFormat sdf;
+		if(game.equals(GameEnum.NA)) {
+			sdf = Scheduler.SDF_NA;
+		}else {
 			sdf = Scheduler.SDF_JP;
 		}
 		
 		int closest = Integer.MAX_VALUE;
 		Date now = new Date();
-		for(String time : start) {
+		for(String time : times) {
 			try {
 				Date luxTime = sdf.parse(time);
 				luxTime.setYear(Calendar.getInstance().get(Calendar.YEAR)-1900);
