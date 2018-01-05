80431da318460686a30ebf9d2d851b8f52e89eff
Noah Gil <xlash19o13@gmail.com>
2018-01-05 16:18:54 -0500

    A medal in all of its glory

---
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/medals/Medal.java
@@ -0,0 +1,37 @@
+package xlash.bot.khux.medals;
+
+public class Medal {
+	
+	public String mid, name, special;
+	public Type type;
+	public Attribute attribute;
+	public Tier tier;
+	public float baseLow, baseHigh, maxLow, maxHigh;
+	public int strength, guages;
+	public Target target;
+	
+	public Medal(String mid, String name, String special, int type, int attribute, int tier, float baseLow, float baseHigh, float maxLow, float maxHigh, int strength, int guages, int target) {
+		this.mid = mid;
+		this.name = name;
+		this.special = special;
+		this.type = Type.getFromId(type);
+		this.attribute = Attribute.getFromId(attribute);
+		this.tier = Tier.getFromTier(tier);
+		this.baseLow = baseLow;
+		this.baseHigh = baseHigh;
+		this.maxLow = maxLow;
+		this.maxHigh = maxHigh;
+		this.strength = strength;
+		this.guages = guages;
+		this.target = Target.getFromId(target);
+	}
+	
+	public boolean equals(Object o) {
+		if(o instanceof Medal) {
+			Medal m = (Medal) o;
+			return m.mid.equals(this.mid);
+		}
+		return false;
+	}
+
+}
