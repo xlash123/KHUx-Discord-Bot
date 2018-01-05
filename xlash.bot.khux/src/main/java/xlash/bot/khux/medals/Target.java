3abd903c219a88bf21274e7af011a7890093e9b9
Noah Gil <xlash19o13@gmail.com>
2018-01-05 16:19:20 -0500

    The target of a medal

---
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/medals/Target.java
@@ -0,0 +1,26 @@
+package xlash.bot.khux.medals;
+
+public enum Target {
+	
+	SINGLE("Single", 0),
+	AOE("All", 1),
+	RANDOM("Random", 2);
+	
+	public String name;
+	public int id;
+	
+	private Target(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	public static Target getFromId(int id) {
+		for(Target t : Target.class.getEnumConstants()) {
+			if(t.id == id) {
+				return t;
+			}
+		}
+		return null;
+	}
+
+}
