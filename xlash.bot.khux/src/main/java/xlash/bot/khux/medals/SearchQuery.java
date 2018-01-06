1669920fc683b9618de7a826f0c0caf8eaf195fa
Noah Gil <xlash19o13@gmail.com>
2018-01-05 20:40:35 -0500

    Added a size method for easy calling

--- xlash.bot.khux/src/main/java/xlash/bot/khux/medals/SearchQuery.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/medals/SearchQuery.java
@@ -6,6 +6,10 @@ public class SearchQuery {
 	
 	public ArrayList<MedalQuery> queries = new ArrayList<>();
 	
+	public int size() {
+		return queries.size();
+	}
+	
 	public class MedalQuery{
 		
 		public String name;
