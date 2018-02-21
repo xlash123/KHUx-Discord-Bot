81addc82f33a9104f44a3d68f8ea49ebd59924e5
xlash <xlash@Noah-L.fios-router.home>
2018-02-20 22:04:38 -0500

    Tries 5 times to get a valid connection with Twitter

--- xlash.bot.khux/src/main/java/xlash/bot/khux/TwitterHandler.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/TwitterHandler.java
@@ -13,15 +13,15 @@ import de.btobastian.javacord.entities.Channel;
  *
  */
 public class TwitterHandler {
-	
+
 	/**
 	 * Stores the last 10 Tweets for NA
 	 */
-	public ArrayList<Tweet> latestTweetsNA = new ArrayList<>();
+	public ArrayList<Tweet> latestTweetsNA;
 	/**
 	 * Stores the last 10 Tweets for JP
 	 */
-	public ArrayList<Tweet> latestTweetsJP = new ArrayList<>();
+	public ArrayList<Tweet> latestTweetsJP;;
 
 	public TwitterHandler() {
 		latestTweetsNA = this.getCurrentTweetList(GameEnum.NA);
@@ -40,44 +40,47 @@ public class TwitterHandler {
 			if (tweet != null)
 				channel.sendMessage(tweet.getLink());
 		}
-		
-		return tweets.size()>0;
+
+		return tweets.size() > 0;
 	}
 
 	/**
 	 * Gets the new Tweets detected
+	 * 
 	 * @param game
-	 * @param updateArchive will this modify the existing archive? If true, then running this command immediately a second time will return nothing.
+	 * @param updateArchive
+	 *            will this modify the existing archive? If true, then running this
+	 *            command immediately a second time will return nothing.
 	 * @return
 	 */
 	public ArrayList<Tweet> getNewTwitterLinks(GameEnum game, boolean updateArchive) {
 		ArrayList<Tweet> ret = new ArrayList<Tweet>();
-		
+
 		ArrayList<Tweet> current = this.getCurrentTweetList(game);
 		ArrayList<Tweet> archive = new ArrayList<>();
-		if(game==GameEnum.NA) {
+		if (game == GameEnum.NA) {
 			archive.addAll(latestTweetsNA);
-		}else {
+		} else {
 			archive.addAll(latestTweetsJP);
 		}
-		
-		for(int i=0; i<current.size(); i++) {
+
+		for (int i = 0; i < current.size(); i++) {
 			Tweet t = current.get(i);
-			if(!archive.contains(t)) {
+			if (!archive.contains(t)) {
 				ret.add(t);
 				archive.add(0, t);
 			}
 		}
-		
-		for(int i=0; i<ret.size(); i++) {
-			archive.remove(archive.size()-1);
+
+		for (int i = 0; i < ret.size(); i++) {
+			archive.remove(archive.size() - 1);
 		}
-		
-		if(updateArchive) {
-			if(game==GameEnum.NA) {
+
+		if (updateArchive) {
+			if (game == GameEnum.NA) {
 				latestTweetsNA.clear();
 				latestTweetsNA.addAll(archive);
-			}else {
+			} else {
 				latestTweetsJP.clear();
 				latestTweetsJP.addAll(archive);
 			}
@@ -94,72 +97,77 @@ public class TwitterHandler {
 	 * @return The URL for the Tweet.
 	 */
 	public Tweet getTwitterUpdateLink(int recent, GameEnum game) {
-		if (recent > 9){
+		if (recent > 9) {
 			return null;
 		}
 		ArrayList<Tweet> archive;
-		if(game==GameEnum.NA) {
+		if (game == GameEnum.NA) {
 			archive = this.latestTweetsNA;
-		}else {
+		} else {
 			archive = this.latestTweetsJP;
 		}
 		return archive.get(recent);
 	}
-	
+
 	/**
 	 * The a current list of the first 10 Tweets listed on their Twitter page.
+	 * 
 	 * @param game
 	 * @return
 	 */
-	public ArrayList<Tweet> getCurrentTweetList(GameEnum game){
-		String gameString = (game == GameEnum.NA ? "kh_ux_na" : "khux_pr");
-		String link = "https://twitter.com/" + gameString + "/";
-		ArrayList<Tweet> ret = new ArrayList<>();
-		try {
-			Document doc = Jsoup.connect(link).get();
-			for(int i=0; i<10; i++) {
-				Elements list = doc.getElementsByClass("stream-items js-navigable-stream").get(0)
-						.getElementsByAttributeValueMatching("data-item-type", "tweet");
-				for(int ii=0; ii<10 && ii<list.size(); ii++) {
-					long tweetId = Long.parseLong(list.get(ii).attr("data-item-id"));
-					ret.add(new Tweet(tweetId, game));
+	public ArrayList<Tweet> getCurrentTweetList(GameEnum game) {
+		int tries = 0;
+		do {
+			String gameString = (game == GameEnum.NA ? "kh_ux_na" : "khux_pr");
+			String link = "https://twitter.com/" + gameString + "/";
+			ArrayList<Tweet> ret = new ArrayList<>();
+			try {
+				Document doc = Jsoup.connect(link).get();
+				for (int i = 0; i < 10; i++) {
+					Elements list = doc.getElementsByClass("stream-items js-navigable-stream").get(0)
+							.getElementsByAttributeValueMatching("data-item-type", "tweet");
+					for (int ii = 0; ii < 10 && ii < list.size(); ii++) {
+						long tweetId = Long.parseLong(list.get(ii).attr("data-item-id"));
+						ret.add(new Tweet(tweetId, game));
+					}
 				}
+				return ret;
+			} catch (Exception e) {
+				tries++;
+				System.err.println("Failed to get update from Twitter.");
+				e.printStackTrace();
 			}
-			return ret;
-		} catch (Exception e) {
-			System.out.println("Failed to get update from Twitter.");
-			e.printStackTrace();
-		}
+		} while (tries <= 5);
 		return null;
 	}
-	
+
 	/**
 	 * A class for storing some info about a tweet
 	 *
 	 */
-	public class Tweet{
-		
+	public class Tweet {
+
 		public final long tweetId;
 		private final GameEnum game;
-		
+
 		public Tweet(long tweetId, GameEnum game) {
 			this.tweetId = tweetId;
 			this.game = game;
 		}
-		
+
 		public String getLink() {
 			String gameString = (game == GameEnum.NA ? "kh_ux_na" : "khux_pr");
 			return "https://twitter.com/" + gameString + "/status/" + tweetId;
 		}
-		
+
 		public boolean equals(Object o) {
-			if(o instanceof Tweet) {
+			if (o instanceof Tweet) {
 				Tweet t = (Tweet) o;
 				return t.tweetId == this.tweetId;
 			}
 			return false;
 		}
-		
+
 	}
 
 }
