5c534526cbe43bdf18eed9dea27402416e1afc0e
xlash <xlash@192.168.1.194>
2018-03-07 21:33:26 -0500

    Added max level

--- xlash.bot.khux/src/main/java/xlash/bot/khux/keyblades/Keyblade.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/keyblades/Keyblade.java
@@ -2,15 +2,28 @@ package xlash.bot.khux.keyblades;
 
 public class Keyblade {
 	
-	public String name;
+	public final String name;
 	/**
 	 * The list of all the slots in the keyblade. Slot 5 is friend
 	 */
-	public Slot[] slots;
+	public final Slot[] slots;
+	
+	private int maxLevel;
 	
 	public Keyblade(String name, Slot[] slots) {
 		this.name = name;
 		this.slots = slots;
+		
+		for(Slot slot : slots) {
+			if(slot.attrActive==0) {
+				this.maxLevel = slot.attrMultipliers.size()-1;
+				break;
+			}
+		}
+	}
+	
+	public int getMaxLevel() {
+		return maxLevel;
 	}
 	
 	public Slot getSlot(int number) {
