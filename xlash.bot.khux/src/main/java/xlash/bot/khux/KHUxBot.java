9d2126689afb6c659c5a73549fbb468faa3f4003
Noah Gil <xlash19o13@gmail.com>
2019-12-08 17:14:26 -0500

    Update dependencies

--- xlash.bot.khux/src/main/java/xlash/bot/khux/KHUxBot.java
+++ xlash.bot.khux/src/main/java/xlash/bot/khux/KHUxBot.java
@@ -53,8 +53,8 @@ import xlash.bot.khux.util.BonusTimes;
  */
 public class KHUxBot {
 
-	public static final String VERSION = "1.11.2";
-	public static final String CHANGELOG = "Updated Javacord API and made some backend changes. KHUxBot should be more stable now.";
+	public static final String VERSION = "1.11.3";
+	public static final String CHANGELOG = "Updated depencencies";
 
 	/** Instance of the Discord API*/
 	public static DiscordApi api;
